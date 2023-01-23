492afe7d4d4d35d108fa72cc7b85fb87b2f7e4dc
WaterFlavoured <106192128+WaterFlavoured@users.noreply.github.com>
2023-01-23 12:27:52 -0500

    testing push

--- Space_Project/src/Project_2DShooting/Main_Control.java
+++ Space_Project/src/Project_2DShooting/Main_Control.java
@@ -42,7 +42,7 @@ public class Main_Control {
 	Timer t = new Timer(10, new Tl1());
 	Panel pnl;
 	
-	// this is a comment
+	// this is a commen
 	
 	AffineTransform tx = AffineTransform.getRotateInstance(Math.toRadians(gunAng1), player.x, player.y);
 	//AffineTransform tx1 = AffineTransform.getRotateInstance(Math.toRadians(gunAng2), enemy.x, enemy.y);
